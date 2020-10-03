744ee14c64e25688092383e7f5b79e357be64abf
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-10-03 17:32:27 -0400

    Sign up module

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class signupForm(forms.Form):
-    fname = forms.CharField()
-    lname = forms.CharField()
-    email = forms.EmailField()
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
