0fb8654b879ae02525277d714e7c2267f52ba4a6
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-10-05 18:33:17 -0400

    Sign up form updated

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('sign-up',views.signup),
-    path('sign-up-test',views.SignUpForm),
+    path('sign-up-test',views.signupForm),
 ]
