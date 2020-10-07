a11f19b5417a6cbacd2323f9023e4689ac3c3109
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-10-07 18:10:35 -0400

    Requierments and White Noise

--- CS490/settings.py
+++ CS490/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = BASE_DIR.joinpath('static/files')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
