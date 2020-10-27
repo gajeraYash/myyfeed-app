e2d2a8a41e732f38a5cc527268128727b505baae
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-10-27 17:10:50 -0400

    Image Upload

--- app/models.py
+++ app/models.py
@@ -41,18 +41,18 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    def get_fields(self):
+        return [(field.name, field.value_to_string(self)) for field in UserProfile._meta.fields]
 
 class UserAnnoucement(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE)
     announcement = models.TextField(max_length=280)
+    image = models.ImageField(upload_to="user_files", blank=True)
     created = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.user.username + "---" + self.announcement
 
-class UserAnnoucementsImage(models.Model):
-    post = models.ForeignKey(UserAnnoucement, on_delete=CASCADE)
-    image = models.ImageField(upload_to="user_files", blank=True)
-
-    def __str__(self):
-        return self.post.user.username
+    def get_queryset(self):
+        print("Get Field Called")
+        return UserAnnoucement.objects.all()
