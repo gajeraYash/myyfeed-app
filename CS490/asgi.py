6495c9ee9d30296852727fa4bee08d4ab1d6572c
moesaifan99 <68472462+moesaifan99@users.noreply.github.com>
2020-11-06 13:32:36 -0500

    Revert "Messenger"

--- CS490/asgi.py
+++ CS490/asgi.py
@@ -10,11 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS490.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-
-})
+application = get_asgi_application()
