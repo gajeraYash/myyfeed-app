481e8117e12811eda02286411ad172443c5c7052
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-11-10 17:04:50 -0500

    Reconfig Channels

--- app/views.py
+++ app/views.py
@@ -18,6 +18,15 @@ def index(request):
 def test_page(request):
     return render(request, 'app/index.html')
 
+def message(request):
+    return render(request, 'app/message.html')
+
+def room(request, room_name):
+    return render(request, 'app/room.html', {
+        'room_name': room_name
+    })
+
+
 @login_required
 def user_search(request):
     user_param = request.GET.get('user_search', None)
