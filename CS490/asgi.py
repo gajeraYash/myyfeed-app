32399d267a1fd55e9967113f92c964de02287ae5
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-11-16 20:23:15 -0500

    Procfile and ASGI Change

--- CS490/asgi.py
+++ CS490/asgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 import app.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS490.settings")
