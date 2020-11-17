ceb562b2736ad4f8f6891f2234273e97af99b8ae
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-11-16 22:45:46 -0500

    Daphne Tested Locally

--- CS490/asgi.py
+++ CS490/asgi.py
@@ -6,26 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
-from channels.layers import get_channel_layer
-from channels.security.websocket import AllowedHostsOriginValidator
-import app.routing
+from channels.routing import get_default_application
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS490.settings")
 django.setup()
 application = get_default_application()
-channel_layer = get_channel_layer()
-
-application = ProtocolTypeRouter({
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                app.routing.websocket_urlpatterns
-            )
-        )
-    ),
-})
