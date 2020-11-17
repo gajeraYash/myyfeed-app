6c672d5ee4f31aee5e85361c012297e2c7a7812d
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-11-16 22:15:37 -0500

    RUN 5

--- CS490/asgi.py
+++ CS490/asgi.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from channels.layers import get_channel_layer
 from channels.security.websocket import AllowedHostsOriginValidator
 import app.routing
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS490.settings")
 django.setup()
+application = get_default_application()
+channel_layer = get_channel_layer()
+
 application = ProtocolTypeRouter({
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
