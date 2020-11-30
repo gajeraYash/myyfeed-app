3f2d72d45e5cfaccc792adfc837650c43303add3
Yash Gajera <34175906+gajeraYash@users.noreply.github.com>
2020-11-30 18:41:48 -0500

    profile integrate user_profile

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,16 +1,23 @@
 function get_feed() {
-    $.ajax({
-        url: 'user/feed',
+    var username = window.location.pathname.split("/").pop();
+    var params = {
+        url: 'feed',
         type: "get",
         cache: true,
         data: {
-            'feed_param': ''
+            'feed_param': username
         },
         dataType: 'html',
         success: function (data) {
             $('#feed-content').html(data)
         }
-    });
+    };
+    if (username == 'profile'){
+        params.url = 'user/feed'
+        params.data.feed_param = ''; 
+    }
+    console.log('DATA FOR:', params.data.feed_param);
+    $.ajax(params);
     // 'feed_param': 'FOLLOWING'  Get USER Following Announcements
     // 'feed_param': ''  Get Self created announcements
     // 'feed_param': 'username'  Get Other user created announcements
